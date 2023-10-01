67b43e61f54de5007ea589ee74d887fbe4d506a7
Ayush20002 <ayushdwivedi20002@gmail.com>
2023-10-01 09:58:17 +0530

    Initial commit

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,61 @@
+import "./Hero.css";
+import CountUp from "react-countup";
+import { motion } from "framer-motion";
+import SearchBar from "../SearchBar/SearchBar";
+const Hero = () => {
+  return (
+    <section className="hero-wrapper">
+      <div className="paddings innerWidth flexCenter hero-container">
+        {/* left side */}
+        <div className="flexColStart hero-left">
+          <div className="hero-title">
+            <div className="orange-circle" />
+            <motion.h1
+            initial={{ y: "2rem", opacity: 0 }}
+            animate={{ y: 0, opacity: 1 }}
+            transition={{
+              duration: 2,
+              type: "ease-in",
+            }}
+            >
+              Find <br />
+              Your Dream
+              <br /> Property
+            </motion.h1>
+          </div>
+          <div className="flexColStart secondaryText flexhero-des">
+            <span>Get a affordable houses</span>
+            <span>Ease your journey to finding the perfect place.</span>
+          </div>
+
+          <SearchBar/>
+
+          <div className="flexCenter stats">
+            
+
+            
+
+            
+          </div>
+        </div>
+
+        {/* right side */}
+        <div className="flexCenter hero-right">
+          <motion.div
+            initial={{ x: "7rem", opacity: 0 }}
+            animate={{ x: 0, opacity: 1 }}
+            transition={{
+              duration: 2,
+              type: "ease-in",
+            }}
+            className="image-container"
+          >
+            <img src="./hero-image.png" alt="houses" />
+          </motion.div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
